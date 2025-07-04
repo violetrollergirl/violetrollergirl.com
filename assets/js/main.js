680dba8dfb975705cc27755bde897446067f5237
Violet Rollergirl <violetrollergirl@riseup.net>
2025-07-04 15:02:04 -0400

    Better CSS reuse, actually SASS, reorder the Contact page.

--- assets/js/main.js
+++ assets/js/main.js
@@ -185,11 +185,11 @@ layout: none
             }, {} );
             var templateText = `Hi {{ site.author | split: " " | first }},
 
-Please allow me to introduce myself. My name is ${inquiryData.booking_inquiry_prospect_name}. I have read your screening requirements and am comfortable being screened by sending you ${inquiryData.booking_inquiry_prospect_preferred_screening_method}.
+Please allow me to introduce myself. My name is ${inquiryData.booking_inquiry_prospect_name}. I read your screening requirements and am comfortable being screened by sending you ${inquiryData.booking_inquiry_prospect_preferred_screening_method}.
 
 My carrier phone number (for your screening purposes only) is ${inquiryData.booking_inquiry_prospect_carrier_phone}.
 
-I've been drawn to you since I found your website via ${inquiryData.booking_inquiry_prospect_source_referral}. I really like your ${inquiryData.booking_inquiry_prospect_attraction}
+I've been drawn to you since I found your website via ${inquiryData.booking_inquiry_prospect_source_referral}. I like your ${inquiryData.booking_inquiry_prospect_attraction}
 
 I'm ready to plan a date! May I reserve your time for ${inquiryData.booking_inquiry_prospect_call_type} for your ${inquiryData.booking_inquiry_prospect_service_type} for a duration of ${inquiryData.booking_inquiry_prospect_duration} hours when I will be in ${inquiryData.booking_inquiry_prospect_location} on ${new Date(inquiryData.booking_inquiry_prospect_preferred_datetime)}, or alternatively, on ${new Date(inquiryData.booking_inquiry_prospect_alternate_datetime)}. Would either of these options allow us to meet?
 
